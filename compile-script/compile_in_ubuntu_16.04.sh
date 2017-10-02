fd3a9007b633c61608d04f58238fb24225b9825d
João Rocha da Silva <joaorosilva@gmail.com>
2017-10-02 22:51:42 +0100

    Update compile_in_ubuntu_16.04.sh

--- compile-script/compile_in_ubuntu_16.04.sh
+++ compile-script/compile_in_ubuntu_16.04.sh
@@ -1,18 +1,41 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 sudo apt-get -y -f -qq install unzip devscripts autoconf automake libtool flex bison gperf gawk m4 make libssl-dev
 
 sudo apt-get -y update
 apt-get install checkinstall build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev
 
-sudo rm -rf virtuoso-opensource
+#sudo rm -rf virtuoso-opensource*
 
-sudo git clone https://github.com/openlink/virtuoso-opensource.git virtuoso-opensource &&
-cd virtuoso-opensource &&
-sudo git branch remotes/origin/develop/7 &&
+while getopts b:f: option
+do
+ case "${option}"
+ in
+ b) BRANCH=${OPTARG};;
+ f) CHECKOUT_FOLDER=${OPTARG};;
+ esac
+done
+
+if [ "${BRANCH}" == "" ]; then
+       echo "No branch specified for build!"
+       exit 1
+else
+       echo "Building branch ${BRANCH}..."
+fi
+
+if [ "${CHECKOUT_FOLDER}" == "" ]; then
+        echo "No CHECKOUT FOLDER  specified for build!"
+        exit 1
+else
+        echo "CHECKING OUT TO FOLDER ${CHECKOUT_FOLDER}..."
+fi
+
+sudo rm -rf "$CHECKOUT_FOLDER"
+sudo git clone https://github.com/openlink/virtuoso-opensource.git "$CHECKOUT_FOLDER"  &&
 sudo ./autogen.sh &&
 CFLAGS="-O2 -m64" &&
 export CFLAGS &&
 sudo ./configure && #> /dev/null 2>&1
 sudo make --silent && #> /dev/null 
-sudo checkinstall
+sudo checkinstall &&
+cp *.deb "../../debs-ubuntu-16-04"
